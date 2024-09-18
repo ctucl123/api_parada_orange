5f260014d9dadae4ab3cf382861f38e0b203d74e
David Diaz <ctucl-desarrollo@outlook.com>
2024-09-18 13:02:19 -0500

    funciona la parada

--- MecanismLogic.py
+++ MecanismLogic.py
@@ -1,27 +1,59 @@
 from gpiosManager import GpiosManager
 import threading
 import time
-
+import subprocess
 
 doors =  GpiosManager()
 
 
-def timer(target_time,delay):   
-    doors.turnstileOpen()
-    inicio = time.time()
-    while time.time() - inicio < target_time:
-        if doors.ReadSensor() == 0:  # Esperar a que el sensor cambie a 0
-            #print("Sensor detecta movimiento (0)")
-            while doors.ReadSensor() == False:
-                time.sleep(0.1)  # Esperar a que el sensor vuelva a 1
-            if doors.ReadSensor():
-                #print("Sensor volvió a 1, desactivando sistema")
-                doors.turnstileBlock()
-                time.sleep(delay)
-                break
-        time.sleep(0.1)
-    #print("termino la ejecucion")
-    doors.turnstileBlock()
+ingreso= "sounds/ingreso.wav"
+retorno= "sounds/retorno.wav"
+def timer(target_time,delay):
+    if doors.ReadSensor() == True:
+        doors.turnstileOpen()
+        inicio = time.time()
+        try:
+            subprocess.run(["aplay",ingreso], check=True)
+        except Exception as e:
+            print("Error audio")
+        while time.time() - inicio < target_time:
+            if doors.ReadSensor() == 0: 
+                timeaux = time.time()
+                while doors.ReadSensor() == False:
+                    time.sleep(0.1) 
+                    if time.time() - timeaux >= target_time:
+                        doors.turnstileBlock()
+                        break
+                if doors.ReadSensor():
+                    doors.turnstileBlock()
+                    time.sleep(delay)
+                    break
+            time.sleep(0.1)
+        doors.turnstileBlock()
+    elif doors.ReadSensor()==False:
+        doors.turnstileBlock()
+        try:
+            subprocess.run(["aplay", retorno], check=True)
+        except Exception as e:
+            print("Error audio")
+        while doors.ReadSensor() == False:  # Esperar hasta que el sensor sea True
+            time.sleep(0.1)
+        doors.turnstileOpen()
+        try:
+            subprocess.run(["aplay", ingreso], check=True)
+        except Exception as e:
+            print("Error audio")
+        inicio = time.time()
+        while time.time() - inicio < target_time:
+            if doors.ReadSensor() == 0:  # Esperar a que el sensor cambie a 0
+                while doors.ReadSensor() == False:
+                    time.sleep(0.1)  # Esperar a que el sensor vuelva a 1
+                if doors.ReadSensor():
+                    doors.turnstileBlock()
+                    time.sleep(delay)
+                    break
+            time.sleep(0.1)
+        doors.turnstileBlock()   
 
 def timerSpecialDoor(target_time,timer_on,timer_off,delay):
     time.sleep(delay)
