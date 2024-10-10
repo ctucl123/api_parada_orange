8ea25e8a675b11d0ff0f63c4e65972e8aa25947f
David Diaz <ctucl-desarrollo@outlook.com>
2024-10-10 16:45:03 -0500

    se realiza una modificacion al archivo api test

--- test/api_test.py
+++ test/api_test.py
@@ -1,16 +1,13 @@
 import requests
 
-url = "http://192.168.1.92:5000/api/mecanism"
 
-# JSON con la operación que se quiere realizar
-json_data = {
-    "operation": "test_lock"
-}
 
-response = requests.post(url, json=json_data)
+url = "http://192.168.1.92:5000/api/database?operation=last_transactions"
 
-if response.status_code == 200:
-    result = response.text
-    print( result)
-else:
-    print(f"Error: {response.status_code}")
+payload = {}
+files={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+print(response.text)
